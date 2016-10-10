deffb966a051bab0139961547fbd538941342934
xxxsergiyxxx <kornilyuk.cergei@gmail.com>
2016-10-10 22:02:01 +0200

    Add project

---
+++ module.js
@@ -0,0 +1,103 @@
+﻿document.addEventListener("DOMContentLoaded", function(e)
+{
+	var btnBook, recBook;
+	var nBook,rBook;
+	btnBook=document.getElementById("new_book");
+	recBook=document.getElementById("rec_book");
+	var k=document.getElementById("img_container");
+	var finish=document.getElementById("books_container");
+	k.addEventListener("dragstart",drag);
+	k.addEventListener("drop",drop);
+	k.addEventListener("dragover", allowDrop);
+	finish.addEventListener("dragstart",drag);
+	finish.addEventListener("drop",drop);
+	finish.addEventListener("dragover", allowDrop);
+	btnBook.addEventListener("click",function(e)
+	{
+		nBook=new ClassBook();
+		nBook.title=document.getElementById("book_name").value;
+		nBook.src=document.getElementById("book_src").value;
+		nBook.type=document.getElementById("book_type").value;
+		nBook.getId(data_book);
+		data_book.push(nBook);
+		AddBookView(nBook);
+	});
+	recBook.addEventListener("click", function(e)
+	{
+		rBook=new Recept();
+		alert(masBookfind);
+		rBook.getIncludes();
+		rBook.title=document.getElementById("rec_name").value;
+		rBook.type=document.getElementById("rec_type").value;		
+		rBook.getId(data_recept);
+		data_recept.push(rBook);
+		AddRecView(rBook);
+	});
+});
+function AddBookView(nBook)
+{
+	var dive=document.getElementById("img_container");
+	dive.innerHTML+='<div draggable="true" class="books_prop" id="'+nBook.type+'_'+nBook.id+'">\n'+
+			'<img class="polk_images" src="'+nBook.src+'">\n'+
+			'</div>\n';
+}
+function AddRecView(rBook)
+{
+	var sel=document.getElementById("select_recept");
+	var d="";
+	for(var i=0, len=data_recept.length;i<len;i++)
+	{
+		d+='<option id="'+data_recept[i].type+'_'+data_recept[i].id+'">'+data_recept[i].title+'</option>';
+	}
+	DeleteBorder();
+	sel.innerHTML=d;
+}
+function allowDrop(ev) 
+{
+    ev.preventDefault();
+}
+
+function drag(e) 
+{
+	var target;
+	if(e.target.tagName.toLowerCase() =="img" || e.target.parentNode.tagName.toLowerCase()=="div")
+	{
+		target=e.target.parentNode;
+	}	
+	else return;
+    e.dataTransfer.setData("text", target.id);
+}
+
+function drop(ev) 
+{
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+	if(ev.target.id.toLowerCase()=="img_container"||ev.target.id.toLowerCase()=="books_container")
+		ev.target.appendChild(document.getElementById(data));
+}
+function ClassBook()
+{
+	this.title=null;
+	this.src=null;
+	this.type=null;
+	this.id=null;	
+}
+function getId(mas)
+{
+	var newId=parseInt(mas[mas.length-1].id)+1;
+	this.id=newId;
+}
+ClassBook.prototype.getId=getId;
+function Recept()
+{
+	this.title=null;
+	this.type=null;
+	this.includes=null;	
+	this.id=null;
+}
+Recept.prototype.getId=getId;
+Recept.prototype.getIncludes=function()
+{
+	this.includes=masBookfind;
+};
+
---
+++ script.js
@@ -0,0 +1,199 @@
+﻿var data_book,data_recept;
+var masBookfind=[];
+function DeleteBorder()
+{
+	var imges=document.getElementById("img_container");
+	for(var i=0,len=imges.childNodes.length;i<len;i++)
+	{
+		if(imges.childNodes[i].nodeName.toLowerCase()=="div")
+		{
+			imges.childNodes[i].childNodes[1].style.border="";
+		}
+	}
+}
+document.addEventListener("DOMContentLoaded", function(e)
+{
+	var masBookReferat=[];//выбранные рефераты
+	function ToServer(elem,url,func)
+	{
+		var dive=document.getElementById(elem);
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url, true);
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState != 4) return;
+			if (xhr.status != 200) 
+			{
+			   alert('Ћшибка ' + xhr.status + ': ' + xhr.statusText);
+			   return;
+			}
+			data=JSON.parse(xhr.responseText);
+			dive.innerHTML=func(data);
+		}
+		xhr.send();
+	}
+	function ParserId(id)//парсинг id на id и тип, передаю id
+	{
+		var type=id.substring(0,id.lastIndexOf("_"));
+		var id=id.substring(id.lastIndexOf("_")+1);
+		var obj={type:type, id:id};
+		return obj;
+	}
+	function FindRecept(elem)//сравнение массива нажатых картинок с массивом рецептов
+	{
+		var d=""
+		var flag;
+		//alert(data_recept[0].includes.indexOf);
+		for(var i=0,len=data_recept.length;i<len;i++)//проходим по массиву рецептов
+		{
+			flag=true;
+			for(var j=0,len1=masBookfind.length;j<len1;j++)//выбранные картинки
+			{
+				
+				if(data_recept[i].includes.indexOf(masBookfind[j])==-1)
+				{
+					flag=false;
+					break;
+				}
+			}
+			if(flag)
+				d+='<option id="'+data.recept[i].type+'_'+data.recept[i].id+'">'+data.recept[i].title+'</option>';
+		}
+		elem.innerHTML=d;
+	}
+	var k=document.getElementById("img_container");
+	ToServer("img_container","vote",function(data)
+	{
+		data_book=data.books;//массив книг	
+		var d="";
+		for(var i=0, len=data_book.length;i<len;i++)
+		{
+			d+='<div draggable="true" class="books_prop" id="'+data.books[i].type+'_'+data.books[i].id+'">\n'+
+			'<img class="polk_images" src="'+data.books[i].src+'">\n'+
+			'</div>\n';
+		}
+		return d;
+	});
+	ToServer("select_recept","recept",function(data)
+	{
+		data_recept=data.recept;//массив рецептов
+		var d="";
+		for(var i=0, len=data_recept.length;i<len;i++)
+		{
+			d+='<option id="'+data_recept[i].type+'_'+data_recept[i].id+'">'+data_recept[i].title+'</option>';
+		}
+		return d;
+	});
+	var listBook=document.getElementById("select_recept");
+	k.addEventListener("click",function(e)//нажатие на картинку
+	{
+		var target;
+		var obj;
+		if(e.target.nodeName.toLowerCase()=="img")
+		{
+			target=e.target;
+			obj=ParserId(target.parentNode.id);
+			if(target.style.border!="")//нажата второй раз
+			{
+				var delel=masBookfind.indexOf(obj.id);
+				if(delel!=-1)
+				{
+					masBookfind.splice(delel,1);
+					FindRecept(listBook);
+				}
+				target.style.border="";
+				//alert(target.parentNode.id);
+			}
+			else//если нажата первый раз
+			{
+				target.style.border="2px solid black";
+				if(masBookfind.indexOf(obj.id)==-1)
+				{
+					masBookfind.push(obj.id);
+					alert(masBookfind);
+				}
+				FindRecept(listBook);
+			}
+		}
+	});
+	var rec=document.getElementById("right_column");
+	rec.addEventListener("click",function(e)
+	{
+		var target;
+		var obj;
+		if(e.target.nodeName.toLowerCase()=="option")
+		{
+			masBookfind=null;
+			masBookfind=[];
+			target=e.target;
+			obj=ParserId(target.id);
+			DeleteBorder();
+			for(var i=0,len=data_recept.length;i<len;i++)
+			{
+				if(data_recept[i].id==obj.id)
+				{
+					for(var j=0,len1=data_recept[i].includes.length;j<len1;j++)
+						for(var k=0,len2=data_book.length;k<len2;k++)
+						{
+							if(data_recept[i].includes[j]==data_book[k].id)
+							{
+								masBookfind.push(data_book[k].id);
+								var book_sel=document.getElementById(data_book[k].type+"_"+data_book[k].id);
+								//alert(book_sel.id);
+								book_sel.firstChild.nextSibling.style.border="2px solid black";
+							}
+						}
+					break;
+				}
+			}
+		}
+	});
+	var finish=document.getElementById("books_container");
+	function RefMasCount()
+	{
+		var flag;
+		for(var i=0, len=data_recept.length;i<len;i++)
+		{
+			flag=false;
+			if(data_recept[i].includes.length<=masBookReferat.length)
+			{
+				for(var j=0;j<data_recept[i].includes.length;j++)
+				{
+					for(var k=0;k<masBookReferat.length;k++)
+					{
+						if(data_recept[i].includes[j]==masBookReferat[k])
+						{
+							flag=true;
+							break;
+						}
+						else
+						{
+							flag=false;
+						}
+					}
+				}
+			}
+			if(flag)
+				alert(data_recept[i].title);
+		}
+	}
+	finish.addEventListener("drop",function(e)
+	{
+		var obj=ParserId(e.dataTransfer.getData("text"));
+		masBookReferat.push(obj.id);
+		RefMasCount();
+		//alert(masBookReferat);
+	});
+	var start=document.getElementById("img_container");
+	start.addEventListener("drop",function(e)
+	{
+		var obj=ParserId(e.dataTransfer.getData("text"));
+		var search=masBookReferat.indexOf(obj.id);
+		if(search>-1)
+		{
+			masBookReferat.splice(search,1);
+			RefMasCount();
+		}
+		alert(masBookReferat);
+	});
+});
+
