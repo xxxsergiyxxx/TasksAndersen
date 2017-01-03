9ebba194fa8c94357104ebb1cb18209da6994234
xxxsergiyxxx <kornilyuk.cergei@gmail.com>
2017-01-03 17:42:53 +0300

    refactoring 2

--- src/module/routing/routing.module.ts
+++ src/module/routing/routing.module.ts
@@ -1,8 +1,8 @@
 import 'angular-ui-router';
 import  * as angular from 'angular';
 import  Provider  from './routing.controller';
-import PropertySearchContainer from '../../components/PropertySearchContainer/propertySearchContainer.module'
-import SearchListContainer from '../../components/SearchListContainer/searchListContainer.module'
+import PropertySearchContainer from '../../components/PropertySearchContainer/propertySearchContainer.module';
+import SearchListContainer from '../../components/SearchListContainer/searchListContainer.module';
 export default angular.module('routing', [
   'ui.router',
   PropertySearchContainer.name,
